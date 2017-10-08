9c35c3b323e1c6ec58ca342922f144691c7ed874
ford @VBox P27K <forgacs.daniel@gmail.com>
2017-10-08 12:06:28 +0100

    cleaned up. added token classes for everything

--- xlsp.py
+++ xlsp.py
@@ -8,34 +8,23 @@ COLOUR_DB = tuple(['colour '+str(k) for
                     k in range(5)])
 
 class Cell(object):
-    def __init__(self, value, idx, prevcell):
-        self.value = value
+    def __init__(self, cellvalue, idx):
+        self.value = cellvalue
         self.idx = idx
-        self.prevcell = prevcell
     def __str__(self):
         objstr = super(Cell, self).__str__()
-        return '<{0:<7}:{1:<5}: {2}>'.format(
+        return '<"{0:<8}":{1:<10}: {2}>'.format(
             str(self.value), type(self).__name__,
             self.idx)
     def __nonzero__(self):
         return bool(self.value)
 
-class Label(Cell):
+class NameLabel(Cell):
     def __nonzero__(self):
-        isgood = True
-        if self.value == NAMELABEL:
-            if self.idx != 0:
-                isgood = False
-        elif self.value == CODELABEL:
-            if self.idx != 1:
-                isgood = False
-            if not self.prevcell:
-                isgood = False
-            else:
-                if self.prevcell.value != NAMELABEL:
-                    isgood = False
-        return isgood
+        return bool(self.idx==0)
 
+class CodeLabel(Cell):
+    pass
 
 class Name(Cell):
     pass
@@ -56,10 +45,10 @@ class XLS(object):
             filename=XLS_TEST)
         self.sheets = ()
         for sheet in table.sheets():
-            initsheet = Sheet(sheet=sheet)
-            if initsheet:
+            newsheet = Sheet(sheet=sheet)
+            if newsheet:
                 self.sheets = self.sheets+(
-                    initsheet,)
+                    newsheet,)
     def __iter__(self):
         return iter(self.sheets)
     def __nonzero__(self):
@@ -71,15 +60,13 @@ class Sheet(object):
         self.maxcoords = sheet.nrows, sheet.ncols
         self.rows = ()
         for rowidx in range(sheet.nrows):
-            initrow = Row(
-                row=sheet.row(rowidx), idx=rowidx)
-            if initrow:
-                self.rows = self.rows + (initrow,)
+            newrow = Row(row=sheet.row(rowidx),
+                idx=rowidx)
+            if newrow:
+                self.rows = self.rows + (newrow,)
     def __str__(self):
         return '<{}:{}:rows {}: cols: {}>'.format(
-            'sheet',
-            self.name,
-            self.maxcoords[0],
+            'sheet', self.name, self.maxcoords[0],
             self.maxcoords[1], )
     def __nonzero__(self):
         return bool(self.rows)
@@ -88,49 +75,42 @@ class Row(object):
     def __init__(self, row, idx):
         self.idx = idx
         self.cells = ()
-        prevcell = None
         for idx, rawcell in enumerate(row):
-            cell = class_selector(
-                cell=rawcell,
-                cellidx=idx,
-                prevcell=prevcell)
-            if cell:
-                self.cells = self.cells+(
-                        cell,)
+            newcell = class_selector(
+                cellvalue=str(rawcell.value),
+                cellidx=idx)
+            self.cells = self.cells+(newcell,)
     def __str__(self):
-        return '<{}:{}>'.format('Row', self.idx)
+        return '<{}:{}: {}>'.format('Row', self.idx,
+            all(self.cells))
     def __nonzero__(self):
-        return bool(self.cells)
+        # return all(self.cells)
+        # return bool(self.cells)
+        return True
     def __iter__(self):
         return iter(self.cells)
 
-def class_selector(cell, cellidx, prevcell):
-    value = str(cell.value)
-    if not value:
+def class_selector(cellvalue, cellidx):
+    if not cellvalue:
         return Cell(
-            value=False,
-            idx=cellidx,
-            prevcell=prevcell)
-    elif value == NAMELABEL:
-        return Label(
-            value=NAMELABEL,
-            idx=cellidx,
-            prevcell=prevcell)
-    elif value == CODELABEL:
-        return Label(
-            value=CODELABEL,
-            idx=cellidx,
-            prevcell=prevcell)
-    elif value in AREAS:
+            cellvalue=False,
+            idx=cellidx,)
+    elif cellvalue == NAMELABEL:
+        return NameLabel(
+            cellvalue=NAMELABEL,
+            idx=cellidx,)
+    elif cellvalue == CODELABEL:
+        return CodeLabel(
+            cellvalue=CODELABEL,
+            idx=cellidx,)
+    elif cellvalue in AREAS:
         return Area(
-            value=value,
-            idx=cellidx,
-            prevcell=prevcell)
+            cellvalue=cellvalue,
+            idx=cellidx,)
     else:
         return Colour(
-            value=value,
-            idx=cellidx,
-            prevcell=prevcell)
+            cellvalue=cellvalue,
+            idx=cellidx,)
 
 
 if __name__ == '__main__':
@@ -141,4 +121,5 @@ if __name__ == '__main__':
             print '\t', row, '\n\t\t',
             for cell in row:
                 print cell,
+            print
             print
