e6dd0dbba680a80879f2eab0139b3704878f4c48
ford @VBox P27K <forgacs.daniel@gmail.com>
2017-10-10 20:57:27 +0100

    update.]

--- xlsp3.py
+++ xlsp3.py
@@ -10,24 +10,21 @@ class Table(object):
     def __init__(self, xls):
         self.sheets = tuple(Sheet(sheet) for sheet in xls)
     def __str__(self):
-        return '\n'.join([str(sheet) for sheet in self.sheets])
+        return '\n'.join(['sheet:'+str(sheet) for sheet in self.sheets])
 
 class Sheet(object):
     def __init__(self, sheet):
         cellrows = tuple(CellRow(row) for row in sheet)
-        self.rows = tuple(self.row_selector(row) for row in cellrows)
-    def __str__(self):
-        return '\n'.join([str(row) for row in self.rows])+'\n'
+        self.rows = tuple(self.row_selector(cellrow=row) for row in cellrows)
     def row_selector(self, cellrow):
         return cellrow
+    def __str__(self):
+        return ''.join(['\n\trow: '+str(row) for row in self.rows])
 
 class CellRow(object):
     def __init__(self, row):
         self.cells = tuple(self.cell_selector(cell, idx)
                         for idx, cell in enumerate(row))
-    def __str__(self):
-        cells = ', '.join([str(cell) for cell in self.cells])
-        return '<{}: {}>'.format(bool(self), cells)
     def cell_selector(self, value, idx):
         if not value:
             cellclass = Empty
@@ -38,6 +35,10 @@ class CellRow(object):
         elif value in AREAS:
             cellclass = AreaCell
         return cellclass(value=value, idx=idx)
+    def __str__(self):
+        return ', '.join([str(cell) for cell in self.cells])
+        # cells = ', '.join([str(cell) for cell in self.cells])
+        # return '<{}: {}>'.format(bool(self), cells)
 
 class Row(object):
     pass
@@ -52,8 +53,8 @@ class Cell(object):
     def __init__(self, value, idx):
         self.value = value
         self.idx = idx
-    def __str__(self):
-        return '<{}. {}>'.format(self.value, bool(self))
+    # def __str__(self):
+    #     return '<{}. {}>'.format(self.value, bool(self))
 
 class Empty(Cell):
     pass
