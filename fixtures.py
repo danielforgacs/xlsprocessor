4aad90da4df20d4d71d88585d66772d0db99a41e
ford @VBox P27K <forgacs.daniel@gmail.com>
2017-10-16 16:25:07 +0100

    added sheet to fixtures.

--- fixtures.py
+++ fixtures.py
@@ -12,9 +12,26 @@ xls = (
         ('name',            'code',             None,               'area 2'),
         ('name',            'code',             'khkjh',            'area 2'),
     ),
+    (
+        (None,              None,               None,               None),
+        ('name',            None,               None,               None),
+        (None,              'name',             None,               None),
+        (None,              None,               'name',             None),
+        ('name',            None,               'name',             None),
+        ('name',            'code',             'area 1',           'area 2'), # OK
+        ('area 1',          'area 2',           'area 3',           'area 4'), # OK
+        ('area 1',          'area 2',           None,               'area 4'),
+        ('name',            None,               'area 1',           'area 2'),
+        ('name',            'code',             None,               'area 2'),
+        ('name',            'code',             'khkjh',            'area 2'),
+    ),
 )
 
 xls_cellclasses = (
+    (
+        ('NameLabelCell',   'CodeLabelCell',    'AreaCell',         'AreaCell'),
+        ('AreaCell',        'AreaCell',         'AreaCell',         'AreaCell'),
+    ),
     (
         ('NameLabelCell',   'CodeLabelCell',    'AreaCell',         'AreaCell'),
         ('AreaCell',        'AreaCell',         'AreaCell',         'AreaCell'),
@@ -26,6 +43,10 @@ xls_celltruth = (
         (True,              True,               True,               True),
         (True,              True,               True,               True),
     ),
+    (
+        (True,              True,               True,               True),
+        (True,              True,               True,               True),
+    ),
 )
 
 xls_rowclasses = (
@@ -33,9 +54,17 @@ xls_rowclasses = (
         'NameAreaRow',
         'AreaRow'
     ),
+    (
+        'NameAreaRow',
+        'AreaRow'
+    ),
 )
 
 xls_ok_rowindexes = (
+    (
+        5,
+        6,
+    ),
     (
         5,
         6,
